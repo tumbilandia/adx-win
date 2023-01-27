79bc8506ca1939b9a527f6011868eeb6337d6a59
ALVARADO STEFANOV Roumen <bb.sopradev@mail.ru>
2023-01-27 06:08:06 +0100

    add new jenkinsfile

--- playbook_sw_installation/software_installation.sh
+++ playbook_sw_installation/software_installation.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-
-# chmod u+x script
-
-set -o nounset ## set -u : exit the script if you try to use an uninitialised variable
-set -o errexit ## set -e : exit the script if any statement returns a non-true return value
-
-ansible-playbook /home/rra/jenkins_workspace/playbook_sw_installation/software_installation.yml -i /home/rra/jenkins_workspace/playbook_sw_installation/hosts
+ansible-playbook /home/rra/jenkins_workspace/software_installation.yml -i /home/rra/jenkins_workspace/hosts
