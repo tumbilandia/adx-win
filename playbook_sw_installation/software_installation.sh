e868805a97a8058bbb111da1e85cbf87ca646f65
ALVARADO STEFANOV Roumen <bb.sopradev@mail.ru>
2023-01-27 06:27:57 +0100

    add new jenkinsfile

--- playbook_sw_installation/software_installation.sh
+++ playbook_sw_installation/software_installation.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ansible-playbook /home/rra/jenkins_workspace/software_installation.yml -i /home/rra/jenkins_workspace/hosts
+ansible-playbook /home/rra/jenkins_workspace/software_installation.yml -i /home/rra/jenkins_workspace/inventory
