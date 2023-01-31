6219d7072c885bbd2b289ea64b47f270b070fb31
ALVARADO STEFANOV Roumen <bb.sopradev@mail.ru>
2023-01-31 09:45:51 +0100

    add new jenkinsfile

--- playbook_sw_installation/software_installation.sh
+++ playbook_sw_installation/software_installation.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-ansible-playbook /home/rra/playbook_sw_installation/software_installation.yml -i /home/rra/playbook_sw_installation/inventory
+echo $1
+echo $2
+ansible-playbook /home/rra/playbook_sw_installation/software_installation.yml -i /home/rra/playbook_sw_installation/inventory --extra-vars "ansible_user=$1 ansible_password=$2"
