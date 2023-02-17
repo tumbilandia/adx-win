fec64a9773096f1d91d95604d64e76c043b3cf29
ALVARADO STEFANOV Roumen <bb.sopradev@mail.ru>
2023-02-17 08:03:39 +0100

    add new jenkinsfile

--- playbook_sw_installation/xx.sh
+++ playbook_sw_installation/xx.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-if [ $1 -eq 0 ]
+# cat xx.sh | sed 's/\r$//' >newscript.sh
+if [ $1 -eq "0" ]
 then
-cp -f software_uninstall.yml software_installation.yml
-elif [ $1 -eq 1 ]
+  cp -f software_uninstall.yml software_installation.yml
+  echo "Playbook prepared to Uninstall software"
+elif [ $1 -eq "1" ]
 then
-cp -f software_install.yml software_installation.yml
+  cp -f software_install.yml software_installation.yml
+  echo "Playbook prepared to Install software"
 else
-echo Wrong parameter: 0 (uninstall), 1 (install)
+  echo "Wrong parameter: 0 (uninstall), 1 (install)"
 fi
