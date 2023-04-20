a102a52edda6bc9f10dab62eb8b0574f60544274
tumbilandia <15730679+tumbilandia@users.noreply.github.com>
2023-04-20 06:18:23 +0200

    Update software_installation.sh

--- playbook_sw_installation/software_installation.sh
+++ playbook_sw_installation/software_installation.sh
@@ -2,4 +2,4 @@
 echo $1
 echo $2
 #ansible-playbook /home/rra/playbook_sw_installation/software_installation.yml -i /home/rra/playbook_sw_installation/inventory --extra-vars "ansible_user=$1 ansible_password=$2"
-ansible-playbook /home/rra/playbook_sw_installation/software_installation.yml
+ansible-playbook software_installation.yml
